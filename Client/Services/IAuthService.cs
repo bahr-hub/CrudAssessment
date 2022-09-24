900c4abfcd0addf09f116ef074e22c719a40667c
Bahr <a.ah.ismail@gmail.com>
2022-09-24 22:28:51 +0200

    Clint App service

---
+++ Client/Services/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using CrudAssessment.Shared.DataModels;
+
+namespace CrudAssessment.Client.Services
+{
+    public interface IAuthService
+    {
+        Task Login(LoginRequest loginRequest);
+        Task Register(RegisterRequest registerRequest);
+        Task Logout();
+        Task<CurrentUser> CurrentUserInfo();
+    }
+}
