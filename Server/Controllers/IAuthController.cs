a18bc840fb49283f4288d044f14ef8a1846d6fbd
Bahr <a.ah.ismail@gmail.com>
2022-09-25 01:11:36 +0200

    some Change

---
+++ Server/Controllers/IAuthController.cs
@@ -0,0 +1,13 @@
+﻿using CrudAssessment.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudAssessment.Server.Controllers
+{
+    public interface IAuthController
+    {
+        Task<IActionResult> Login(UserInfo request);
+        Task<IActionResult> Register(UserInfo parameters);
+        Task<IActionResult> Logout();
+        UserInfo CurrentUserInfo();
+    }
+}
